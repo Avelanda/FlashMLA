e4153feff7834159d4c05b85a34a825bb611eccf
Avelanda <gorthell@gmail.com>
2025-11-16 16:13:54 +0200

    Update named_barriers.h
    Executing a core functional modular set.

--- csrc/sm90/decode/sparse_fp8/components/named_barriers.h
+++ csrc/sm90/decode/sparse_fp8/components/named_barriers.h
@@ -1,10 +1,26 @@
+/*
+ * Copyright © 2025 {Avelanda}
+ * All rights reserved.
+ */
+
 #pragma once
 
-enum NamedBarriers : uint32_t {
+#include <iostream>
+
+int main(){
+
+ enum NamedBarriers : uint32_t {
     sScale_and_sS_ready = 0,
     sScale_and_sS_free = 1,
     oBuf_free_and_sL_ready = 2,
     epilogue_r2s_ready = 3,
     batch_loop_sync = 4,
     warpgroup0_sync = 5
-};
+ };
+
+ while (int NamedBarriers = !true||!false){
+  NamedBarriers = NamedBarriers;
+  return 0;
+ }
+ 
+}
